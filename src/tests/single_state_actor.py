ff75eeab971d7396acffa7fd3cb33ae31b4badee
Aaron <aaronrmm@gmail.com>
2022-05-14 16:07:48 -0400

    Tests: Separate testing if game is winnable vs learnable

--- src/tests/single_state_actor.py
+++ src/tests/single_state_actor.py
@@ -21,18 +21,42 @@ class TestCase_SingleStateActorValueBandit_Plays_RememberTheNumber(unittest.Test
 
     def test_scoring_is_possible(self):
         """
-        Assert the game can be won, but is not won every time by a random actor
+        Assert the game can be won, but is not won every time by the actor
         """
         game = RememberTheNumber()
         player = SingleStateActorValueBandit(action_space=game.get_action_space())
-        games_to_play = 10000
+        games_to_play = 100
         scores = []
         for _ in tqdm(range(games_to_play)):
             score = game.act(player.get_action(observations=None))
             scores.append(score)
             player.give_feedback(score)
-        assert sum(scores) > games_to_play / 4
-        assert sum(scores) < games_to_play
+        print("Scored: " + str(sum(scores)))
+        assert sum(scores) > 1, "Game may not be winnable"
+        assert sum(scores) < games_to_play, "Game may not be losable"
+
+    def test_game_is_learnable(self):
+        """
+        Assert the game can be learned by an AI
+        """
+        game = RememberTheNumber()
+        trials = 10
+        winners = 0
+        losers = 0
+        for _ in range(trials):
+            player = SingleStateActorValueBandit(action_space=game.get_action_space())
+            games_to_play = 1000
+            scores = []
+            for _ in tqdm(range(games_to_play)):
+                score = game.act(player.get_action(observations=None))
+                scores.append(score)
+                player.give_feedback(score)
+            print("Scored: " + str(sum(scores)))
+            if sum(scores) > games_to_play / 3:
+                winners += 1
+            else:
+                losers += 1
+        assert winners > 0
 
 
 if __name__ == "__main__":
