a72d9a5e6b3399282b46632da180df33e315e2f6
Aaron <aaronrmm@gmail.com>
2022-05-24 12:42:18 -0400

    env test add blackjack test

--- src/tests/test_gym.py
+++ src/tests/test_gym.py
@@ -16,3 +16,16 @@ class TestCase_GymCartPole(unittest.TestCase):
                 observation, info = env.reset(return_info=True)
         env.close()
         assert len(observation) > 0
+
+    def test_blackjack_runs(self):
+        env = gym.make("Blackjack-v1")
+        observation, info = env.reset(seed=42, return_info=True)
+
+        for _ in range(100):
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+
+            if done:
+                observation, info = env.reset(return_info=True)
+        env.close()
+        assert len(observation) > 0
